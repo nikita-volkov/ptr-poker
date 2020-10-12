b5935d20aa258924ca967e333e5f268b691543c9
Nikita Volkov <nikita.y.volkov@mail.ru>
2020-10-13 00:06:51 +0300

    Inline a bunch of functions

--- library/PtrPoker.hs
+++ library/PtrPoker.hs
@@ -37,24 +37,24 @@ concat :: Foldable f => f Poker -> Poker
 concat pokers =
   Poker (\ p -> foldM (\ p (Poker io) -> io p) p pokers)
 
-{-# INLINE byteString #-}
+{-# INLINE[1] byteString #-}
 byteString :: ByteString -> Poker
 byteString bs =
   Poker $ \ ptr -> ByteStringIO.pokeByteString ptr bs
 
-{-# INLINE word8 #-}
+{-# INLINE[1] word8 #-}
 word8 :: Word8 -> Poker
 word8 a =
   Poker (\ p -> PrimIO.pokeWord8 p a $> plusPtr p 1)
 
 {-| Little-endian Word64 poker. -}
-{-# INLINE lWord64 #-}
+{-# INLINE[1] lWord64 #-}
 lWord64 :: Word64 -> Poker
 lWord64 a =
   Poker (\ p -> PrimIO.pokeLEWord64 p a $> plusPtr p 8)
 
 {-| Big-endian Word64 poker. -}
-{-# INLINE bWord64 #-}
+{-# INLINE[1] bWord64 #-}
 bWord64 :: Word64 -> Poker
 bWord64 a =
   Poker (\ p -> PrimIO.pokeBEWord64 p a $> plusPtr p 8)
@@ -63,52 +63,52 @@ bWord64 a =
 -- * ASCII integers
 -------------------------
 
-{-# INLINE asciiDecInt8 #-}
+{-# INLINE[1] asciiDecInt8 #-}
 asciiDecInt8 :: Int8 -> Poker
 asciiDecInt8 a =
   Poker (AsciiIO.pokeIntInDec (fromIntegral a))
 
-{-# INLINE asciiDecInt16 #-}
+{-# INLINE[1] asciiDecInt16 #-}
 asciiDecInt16 :: Int16 -> Poker
 asciiDecInt16 a =
   Poker (AsciiIO.pokeIntInDec (fromIntegral a))
 
-{-# INLINE asciiDecInt32 #-}
+{-# INLINE[1] asciiDecInt32 #-}
 asciiDecInt32 :: Int32 -> Poker
 asciiDecInt32 a =
   Poker (AsciiIO.pokeIntInDec (fromIntegral a))
 
-{-# INLINE asciiDecInt64 #-}
+{-# INLINE[1] asciiDecInt64 #-}
 asciiDecInt64 :: Int64 -> Poker
 asciiDecInt64 a =
   Poker (AsciiIO.pokeLongLongIntInDec (fromIntegral a))
 
-{-# INLINE asciiDecInt #-}
+{-# INLINE[1] asciiDecInt #-}
 asciiDecInt :: Int -> Poker
 asciiDecInt a =
   Poker (AsciiIO.pokeLongLongIntInDec (fromIntegral a))
 
-{-# INLINE asciiDecWord8 #-}
+{-# INLINE[1] asciiDecWord8 #-}
 asciiDecWord8 :: Word8 -> Poker
 asciiDecWord8 a =
   Poker (AsciiIO.pokeUIntInDec (fromIntegral a))
 
-{-# INLINE asciiDecWord16 #-}
+{-# INLINE[1] asciiDecWord16 #-}
 asciiDecWord16 :: Word16 -> Poker
 asciiDecWord16 a =
   Poker (AsciiIO.pokeUIntInDec (fromIntegral a))
 
-{-# INLINE asciiDecWord32 #-}
+{-# INLINE[1] asciiDecWord32 #-}
 asciiDecWord32 :: Word32 -> Poker
 asciiDecWord32 a =
   Poker (AsciiIO.pokeUIntInDec (fromIntegral a))
 
-{-# INLINE asciiDecWord64 #-}
+{-# INLINE[1] asciiDecWord64 #-}
 asciiDecWord64 :: Word64 -> Poker
 asciiDecWord64 a =
   Poker (AsciiIO.pokeLongLongUIntInDec (fromIntegral a))
 
-{-# INLINE asciiDecWord #-}
+{-# INLINE[1] asciiDecWord #-}
 asciiDecWord :: Word -> Poker
 asciiDecWord a =
   Poker (AsciiIO.pokeLongLongUIntInDec (fromIntegral a))
