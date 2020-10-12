073e60ad041c7e5219da42b9862333507df1b024
Nikita Volkov <nikita.y.volkov@mail.ru>
2020-10-12 22:12:57 +0300

    Change the formatting of FFI

--- library/PtrPoker/IO/Ascii.hs
+++ library/PtrPoker/IO/Ascii.hs
@@ -5,20 +5,20 @@ import PtrPoker.Prelude
 import Foreign.C.Types
 
 
-foreign import ccall unsafe "static int_dec" pokeIntInDec
-    :: CInt -> Ptr Word8 -> IO (Ptr Word8)
+foreign import ccall unsafe "static int_dec"
+  pokeIntInDec :: CInt -> Ptr Word8 -> IO (Ptr Word8)
 
-foreign import ccall unsafe "static long_long_int_dec" pokeLongLongIntInDec
-    :: CLLong -> Ptr Word8 -> IO (Ptr Word8)
+foreign import ccall unsafe "static long_long_int_dec"
+  pokeLongLongIntInDec :: CLLong -> Ptr Word8 -> IO (Ptr Word8)
 
-foreign import ccall unsafe "static uint_dec" pokeUIntInDec
-    :: CUInt -> Ptr Word8 -> IO (Ptr Word8)
+foreign import ccall unsafe "static uint_dec"
+  pokeUIntInDec :: CUInt -> Ptr Word8 -> IO (Ptr Word8)
 
-foreign import ccall unsafe "static long_long_uint_dec" pokeLongLongUIntInDec
-    :: CULLong -> Ptr Word8 -> IO (Ptr Word8)
+foreign import ccall unsafe "static long_long_uint_dec"
+  pokeLongLongUIntInDec :: CULLong -> Ptr Word8 -> IO (Ptr Word8)
 
-foreign import ccall unsafe "static uint_hex" pokeUIntInHex
-    :: CUInt -> Ptr Word8 -> IO (Ptr Word8)
+foreign import ccall unsafe "static uint_hex"
+  pokeUIntInHex :: CUInt -> Ptr Word8 -> IO (Ptr Word8)
 
-foreign import ccall unsafe "static long_long_uint_hex" pokeLongLongUIntInHex
-    :: CULLong -> Ptr Word8 -> IO (Ptr Word8)
+foreign import ccall unsafe "static long_long_uint_hex"
+  pokeLongLongUIntInHex :: CULLong -> Ptr Word8 -> IO (Ptr Word8)
