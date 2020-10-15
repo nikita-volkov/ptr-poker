16870ca68c89798dfc0e6552a0d9307b971f7874
Nikita Volkov <nikita.y.volkov@mail.ru>
2020-10-15 19:06:31 +0300

    Add size test

--- test/Main.hs
+++ test/Main.hs
@@ -81,6 +81,13 @@ prop_nonRealZeroNonRealDoubleAsciiDec =
     annotate string
     read string === 0
 
+prop_sizeOfTextUtf8 =
+  withTests 999 $
+  property $ do
+    a <- forAll (Gen.text (Range.exponential 0 9999) Gen.unicode)
+    Size.textUtf8 a
+      === Char8ByteString.length (Text.encodeUtf8 a)
+
 prop_textUtf8 =
   withTests 999 $
   property $ do
