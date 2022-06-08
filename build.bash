e00d6710cf4075ca025fc7985eaf40f9aa889b3a
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-06-08 13:59:41 +0300

    Update the build setup

--- build.bash
+++ build.bash
@@ -1,9 +1,52 @@
 #!/bin/bash
 set -eo pipefail
 
-ormolu --mode inplace -ce \
-$(find . -name "*.hs" \
-  -not -path "./*.stack-work/*" \
-  -not -path "./.git/*")
+function format {
+  ormolu --mode inplace -ce \
+  $(find . -name "*.hs" \
+    -not -path "./.git/*" \
+    -not -path "./*.stack-work/*" \
+    -not -path "./dist/*" \
+    -not -path "./dist-newstyle/*" \
+    -not -path "./samples/*" \
+    -not -path "./sketches/*" \
+    -not -path "./output/*" \
+    -not -path "./ideas/*" \
+    -not -path "./refs/*" \
+    -not -path "./temp/*")
+}
 
-stack build --fast --test
+function build_and_test {
+  stack build \
+  --fast --test \
+  --ghc-options "-j +RTS -A128m -n2m -RTS -fwarn-incomplete-patterns"
+}
+
+function build_and_test_by_pattern {
+  stack build \
+  --fast --test \
+  --ghc-options "-j +RTS -A128m -n2m -RTS -fwarn-incomplete-patterns" \
+  --ta "-p \"$1\""
+}
+
+function build {
+  stack build \
+  --ghc-options "-j +RTS -A128m -n2m -RTS -fwarn-incomplete-patterns" \
+  --fast
+}
+
+function build_failing_on_incomplete_patterns {
+  stack build \
+  --ghc-options "-j +RTS -A128m -n2m -RTS -Werror=incomplete-patterns" \
+  --fast
+}
+
+function install {
+  stack \
+  --work-dir ".install.stack-work" \
+  install \
+  --ghc-options "-j +RTS -A128m -n2m -RTS"
+}
+
+format
+build_and_test
