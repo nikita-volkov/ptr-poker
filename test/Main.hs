cc1f0ec6411833e82308e552ab69e10c0c9a3a52
Nikita Volkov <nikita.y.volkov@mail.ru>
2023-02-18 13:13:16 +0300

    Add tests for integers

--- test/Main.hs
+++ test/Main.hs
@@ -2,12 +2,14 @@
 
 module Main where
 
+import qualified Data.ByteString.Builder as ByteStringBuilder
 import qualified Data.ByteString.Char8 as Char8ByteString
 import qualified Data.Text.Encoding as Text
 import Hedgehog
 import qualified Hedgehog.Gen as Gen
 import Hedgehog.Main
 import qualified Hedgehog.Range as Range
+import IsomorphismClass
 import qualified Numeric.Limits as NumericLimits
 import qualified PtrPoker.Size as Size
 import qualified PtrPoker.Write as Write
@@ -94,6 +96,84 @@ prop_textUtf8 =
     a <- forAll (Gen.text (Range.exponential 0 9999) (Gen.choice [Gen.ascii, Gen.unicode]))
     Write.writeToByteString (Write.textUtf8 a)
       === Text.encodeUtf8 a
+
+prop_word8 =
+  property $ do
+    a <- forAll (Gen.word8 Range.constantBounded)
+    Write.writeToByteString (Write.word8 a)
+      === to (ByteStringBuilder.word8 a)
+
+prop_lWord16 =
+  property $ do
+    a <- forAll (Gen.word16 Range.constantBounded)
+    Write.writeToByteString (Write.lWord16 a)
+      === to (ByteStringBuilder.word16LE a)
+
+prop_bWord16 =
+  property $ do
+    a <- forAll (Gen.word16 Range.constantBounded)
+    Write.writeToByteString (Write.bWord16 a)
+      === to (ByteStringBuilder.word16BE a)
+
+prop_lWord32 =
+  property $ do
+    a <- forAll (Gen.word32 Range.constantBounded)
+    Write.writeToByteString (Write.lWord32 a)
+      === to (ByteStringBuilder.word32LE a)
+
+prop_bWord32 =
+  property $ do
+    a <- forAll (Gen.word32 Range.constantBounded)
+    Write.writeToByteString (Write.bWord32 a)
+      === to (ByteStringBuilder.word32BE a)
+
+prop_lWord64 =
+  property $ do
+    a <- forAll (Gen.word64 Range.constantBounded)
+    Write.writeToByteString (Write.lWord64 a)
+      === to (ByteStringBuilder.word64LE a)
+
+prop_bWord64 =
+  property $ do
+    a <- forAll (Gen.word64 Range.constantBounded)
+    Write.writeToByteString (Write.bWord64 a)
+      === to (ByteStringBuilder.word64BE a)
+
+prop_lInt16 =
+  property $ do
+    a <- forAll (Gen.int16 Range.constantBounded)
+    Write.writeToByteString (Write.lInt16 a)
+      === to (ByteStringBuilder.int16LE a)
+
+prop_bInt16 =
+  property $ do
+    a <- forAll (Gen.int16 Range.constantBounded)
+    Write.writeToByteString (Write.bInt16 a)
+      === to (ByteStringBuilder.int16BE a)
+
+prop_lInt32 =
+  property $ do
+    a <- forAll (Gen.int32 Range.constantBounded)
+    Write.writeToByteString (Write.lInt32 a)
+      === to (ByteStringBuilder.int32LE a)
+
+prop_bInt32 =
+  property $ do
+    a <- forAll (Gen.int32 Range.constantBounded)
+    Write.writeToByteString (Write.bInt32 a)
+      === to (ByteStringBuilder.int32BE a)
+
+prop_lInt64 =
+  property $ do
+    a <- forAll (Gen.int64 Range.constantBounded)
+    Write.writeToByteString (Write.lInt64 a)
+      === to (ByteStringBuilder.int64LE a)
+
+prop_bInt64 =
+  property $ do
+    a <- forAll (Gen.int64 Range.constantBounded)
+    Write.writeToByteString (Write.bInt64 a)
+      === to (ByteStringBuilder.int64BE a)
 
 -- * Gens
 
